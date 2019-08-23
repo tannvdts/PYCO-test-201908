6d0c557d17b2d2bd9b5284ad993286524f2e9134
Tan Nguyen <tannv.dts@gmail.com>
2019-08-23 18:41:53 +0700

    commit rotatePicture function and unit test for it

--- func/rotatePicture.js
+++ func/rotatePicture.js
@@ -1,5 +1,59 @@
 module.exports = (grid, k) => {
-    if (!grid || !k) throw new TypeError('params is required');
+    // check params must be provided
+    if (grid === undefined && k === undefined) throw new TypeError('params is required');
+    // check grid param must be array
     if (!Array.isArray(grid)) throw new TypeError('grid must be array');
+    
+    // check grid param must be n*n matrix
+    const gridError = new RangeError('grid must be n*n matrix');
+    const numRow = grid.length;
+    if (numRow < 1) throw gridError;
+    grid.forEach((row) => {
+        const numCol = row.length;
+        if (numCol !== numRow) {
+            throw gridError;
+        }
+    })
+
+    // check k must be integer >= 0
     if (!Number.isInteger(k) || (Number.isInteger(k) && k < 0)) throw new TypeError('k must be integer >= 0');
+
+    // We really have only 4 rotation state of the picture: 
+    // 0: initial state of picture
+    // 1: equivalent to one-time rotation
+    // 2: equivalent to two-time rotation
+    // 3: equivalent to three-time rotation
+    const type = k % 4;
+
+    switch (type) {
+        case 0:
+            return grid;
+        case 1: {
+            const result = [];
+            for (let i = 0; i < numRow; i++) {
+                result.push(
+                    grid.reduceRight((arr, row) => {
+                        arr.push(row[i]);
+                        return arr;
+                    }, [])
+                );
+            }
+            return result;
+        }
+        case 2: {
+            return grid.map((row) => {
+                return row.reverse();
+            }).reverse();
+        }
+        case 3: {
+            const result = [];
+            for (let i = numRow - 1; i > -1; i--) {
+                result.push(
+                    grid.map((row) => row[i])
+                );
+            }
+            return result;
+        }
+        default: return
+    }
 }
--- test/index.js
+++ test/index.js
@@ -6,7 +6,9 @@ describe('#PYCO Test 201908', () => {
   describe('#rotatePicture(grid, k)', () => {
 
     it('should throw exception when grid or k not provided', () => {
-        expect(rotatePicture).to.throw(TypeError, 'params is required');
+        expect(() => {
+            rotatePicture();
+        }).to.throw(TypeError, 'params is required');
     });
 
     it('should throw exception when grid param is not array', () => {
@@ -17,8 +19,145 @@ describe('#PYCO Test 201908', () => {
 
     it('should throw exception when grid param is not interger >=0', () => {
         expect(() => {
-            rotatePicture([], -1);
+            rotatePicture([[1]], -1);
         }).to.throw(TypeError, 'k must be integer >= 0');
-    })
+    });
+
+    it('should throw exception when grid is not n*n matrix', () => {
+
+        // case empty grid
+        expect(() => {
+            rotatePicture([
+                []
+            ], 0);
+        }).to.throw(RangeError);
+
+        // case row > col
+        expect(() => {
+            rotatePicture([
+                [3],
+                [1]
+            ], 0);
+        }).to.throw(RangeError);
+
+        // case col > row
+        expect(() => {
+            rotatePicture([
+                [3, 2]
+            ], 0);
+        }).to.throw(RangeError);
+        
+        // case number of item in rows not equal
+        expect(() => {
+            rotatePicture([
+                [3, 2],
+                [3, 4, 5]
+            ], 0);
+        }).to.throw(RangeError);
+    });
+
+    // rotate_case_0 : k%4 = 0 (k = 0)
+    it('should return success result in rotate_case_0 (k = 0)', () => {
+        let result =  rotatePicture([
+            [0, 16, 255],
+            [8, 128, 32],
+            [0, 0, 0]
+        ], 0);
+        expect(result).to.deep.equal([
+            [0, 16, 255],
+            [8, 128, 32],
+            [0, 0, 0]
+        ]);
+    });
+    it('should return success result in rotate_case_0 (k = 4)', () => {
+        let result =  rotatePicture([
+            [0, 16, 255],
+            [8, 128, 32],
+            [0, 0, 0]
+        ], 4);
+        expect(result).to.deep.equal([
+            [0, 16, 255],
+            [8, 128, 32],
+            [0, 0, 0]
+        ]);
+    });
+
+    // rotate_case_1 : k%4 = 1 (k = 1)
+    it('should return success result in rotate_case_1 (k = 1)', () => {
+        let result =  rotatePicture([
+            [0, 16, 255],
+            [8, 128, 32],
+            [0, 0, 0]
+        ], 1);
+        expect(result).to.deep.equal([
+            [0, 8, 0],
+            [0, 128, 16],
+            [0, 32, 255]
+        ]);
+    });
+    it('should return success result in rotate_case_1 (k = 5)', () => {
+        let result =  rotatePicture([
+            [0, 16, 255],
+            [8, 128, 32],
+            [0, 0, 0]
+        ], 5);
+        expect(result).to.deep.equal([
+            [0, 8, 0],
+            [0, 128, 16],
+            [0, 32, 255]
+        ]);
+    });
+
+    // rotate_case_2 : k%4 = 2
+    it('should return success result in rotate_case_2 (k = 2)', () => {
+        let result =  rotatePicture([
+            [0, 16, 255],
+            [8, 128, 32],
+            [0, 0, 0]
+        ], 2);
+        expect(result).to.deep.equal([
+            [0, 0, 0],
+            [32, 128, 8],
+            [255, 16, 0]
+        ]);
+    });
+    it('should return success result in rotate_case_2 (k = 6)', () => {
+        let result =  rotatePicture([
+            [0, 16, 255],
+            [8, 128, 32],
+            [0, 0, 0]
+        ], 6);
+        expect(result).to.deep.equal([
+            [0, 0, 0],
+            [32, 128, 8],
+            [255, 16, 0]
+        ]);
+    });
+
+    // rotate_case_3 : k%4 = 3
+    it('should return success result in rotate_case_3 (k = 3)', () => {
+        let result =  rotatePicture([
+            [0, 16, 255],
+            [8, 128, 32],
+            [0, 0, 0]
+        ], 3);
+        expect(result).to.deep.equal([
+            [255, 32, 0],
+            [16, 128, 0],
+            [0, 8, 0]
+        ]);
+    });
+    it('should return success result in rotate_case_3 (k = 7)', () => {
+        let result =  rotatePicture([
+            [0, 16, 255],
+            [8, 128, 32],
+            [0, 0, 0]
+        ], 7);
+        expect(result).to.deep.equal([
+            [255, 32, 0],
+            [16, 128, 0],
+            [0, 8, 0]
+        ]);
+    });
   });
 });
